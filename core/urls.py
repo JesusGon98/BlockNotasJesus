0c88f2bc96c63b84756bc3993957fbd15f7dd739
Jesus Gonzalez <Jesus.gonzalez.199817@gmail.com>
2026-05-21 14:02:27 -0700

    Primer commit API REST blog de notas Jesus

---
+++ core/urls.py
@@ -0,0 +1,32 @@
+from django.http import JsonResponse
+from django.urls import include, path
+from rest_framework import routers
+
+from tasks.views import (
+    UserViewSet,
+    GroupViewSet,
+    TaskViewSet
+)
+
+router = routers.DefaultRouter()
+
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'tasks', TaskViewSet)
+
+
+def health(request):
+    return JsonResponse({"status": "ok"})
+
+
+urlpatterns = [
+    path('health/', health),
+    path('', include(router.urls)),
+    path(
+        'api-auth/',
+        include(
+            'rest_framework.urls',
+            namespace='rest_framework'
+        )
+    ),
+]
---
+++ tasks/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Task(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=100, blank=False)
+    completed = models.BooleanField(default=False)
+
+    owner = models.ForeignKey(
+        "auth.User",
+        related_name="tasks",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["created"]
+
+    def __str__(self):
+        return self.description
